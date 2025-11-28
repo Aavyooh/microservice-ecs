ea64352c760b2ca8b5363a8c092ef88b4f31dcec
Aavyooh <57136508+Aavyooh@users.noreply.github.com>
2025-11-28 08:49:27 +0530

    Add IAM policies for ECS task and execution roles
    Added IAM policies for ECS task and execution roles to manage secrets and logging.

--- iam.tf
+++ iam.tf
@@ -27,7 +27,60 @@ resource "aws_iam_role_policy_attachment" "ecs_full_access_attach" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonECS_FullAccess"
 }
 
-########## ECS Task Execution Role with Standard Assume Role Policy
+resource "aws_iam_policy" "ecs_task_secrets_policy" {
+  name        = "ecs-task-secrets"
+  description = "Allow ECS task to read DB secrets"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "secretsmanager:GetSecretValue",
+          "secretsmanager:DescribeSecret"
+        ]
+        Resource = aws_secretsmanager_secret.db_credentials.arn
+      }
+    ]
+  })
+}
+
+###################### Attach ECS Task Logging Policy ######################
+resource "aws_iam_role_policy_attachment" "ecs_task_secrets_attach" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = aws_iam_policy.ecs_task_secrets_policy.arn
+}
+
+resource "aws_iam_policy" "ecs_execution_logs_policy" {
+  name        = "ecs-execution-logs-policy"
+  description = "Allow ECS tasks to write logs to CloudWatch"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_logs_policy_attach" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = aws_iam_policy.ecs_execution_logs_policy.arn
+}
+
+
+###################################################################################### 
+################## ECS Task Execution Role with Standard Assume Role Policy ################
+
 resource "aws_iam_role" "ecs_task_execution_role" {
   name = "ecs_task_execution_role"
 
@@ -54,4 +107,28 @@ resource "aws_iam_role" "ecs_task_execution_role" {
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# ADD PERMISSION TO READ SECRETS
+resource "aws_iam_policy" "ecs_execution_secrets_policy" {
+  name        = "ecs-execution-secrets"
+  description = "Allow ECS execution role to get secrets"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "secretsmanager:GetSecretValue"
+        ]
+        Resource = aws_secretsmanager_secret.db_credentials.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_secrets_attach" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = aws_iam_policy.ecs_execution_secrets_policy.arn
 }
